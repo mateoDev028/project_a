b9d32330c43e1ed5b3056a82b6fbf611f24553a3
MateoDev <mateodev028@gmail.com>
2022-10-24 23:20:50 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
+import React, { useState, useEffect } from 'react';
 import Header from './routes/header/Header'
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1500)
+  },[])
+
   return (
     <div className="app">
-      <div className="app-navbar">
-        <Navbar />
-      </div>
-      <div className="app-header">
-        <Header />
-      </div>
-      <div className="app-header">
-        <Header />
-      </div>
+      {loading ? (
+          <div className='app-modal'>
+            <h1>CARGANDO...</h1>
+          </div>
+        )
+        : (
+          <div>
+            <div className="app-navbar">
+              <Navbar />
+            </div>
+            <div className="app-header">
+              <Header />
+            </div>
+            <div className="app-header">
+              <Header />
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 }
